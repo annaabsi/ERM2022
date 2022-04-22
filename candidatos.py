88a668bd6c7ccafbee1107ee63d28603ccfb1472
Anna Absi <anna.absi@gmail.com>
2022-04-22 01:57:55 -0500

    initial commit

---
+++ candidatos.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import requests
+
+# LISTA DE CANDIDATOS - NACIONAL
+
+for id_solicitud_lista in range(16860):
+    url = f'https://apiplataformaelectoral.jne.gob.pe/api/v1/candidato/candidatos-lista-internas/{id_solicitud_lista}'
+    resp = requests.get(url=url, verify=False)
+    data = resp.json()['data']
+    if data != []:
+        df = pd.DataFrame(data)
+        df = df.drop(['archivoBase64'], axis=1)
+        df['idSolicitudLista'] = id_solicitud_lista
+        df.to_csv(f"listas/{id_solicitud_lista}.csv")
