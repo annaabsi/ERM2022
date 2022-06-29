b31758af0ced296848787e8b719a3e915256b0aa
Anna Absi <anna.absi@gmail.com>
2022-06-28 19:06:58 -0500

    solo procesos electorales

--- infogob.py
+++ infogob.py
@@ -9,44 +9,44 @@ session = HTMLSession()
 
 # LISTAR CANDIDATOS
 
-candidatos = pd.read_csv('alcaldes_provinciales.csv')
+# candidatos = pd.read_csv('alcaldes_provinciales.csv')
 
-dict = {}
-for dni_candidato in candidatos['txDocumentoIdentidad']:
-    try:
-        url = "https://infogob.jne.gob.pe/Politico/ListarPolitico"
-        headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:66.0) Gecko/20100101 Firefox/66.0"}
-        body = {"IdDNI": dni_candidato, "token":"cA6%2B6LtErj8=6L"}
-        resp = requests.post(url=url, headers=headers, data=body, verify=False)
-        data = resp.json()["Data"][0]["TxRutaPolitico"].split("partidario_",1)[1]
-        data_encoded = urllib.parse.quote(data.encode('utf8'))
-        dict[dni_candidato] = data_encoded
-        print(data_encoded)
-    except:
-        continue
-df = pd.DataFrame(dict.items(), columns=["DNI","URL"])
-df.to_csv("dnis_urls_alcaldes_provinciales.csv")
+# dict = {}
+# for dni_candidato in candidatos['txDocumentoIdentidad']:
+#     try:
+#         url = "https://infogob.jne.gob.pe/Politico/ListarPolitico"
+#         headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:66.0) Gecko/20100101 Firefox/66.0"}
+#         body = {"IdDNI": dni_candidato, "token":"cA6%2B6LtErj8=6L"}
+#         resp = requests.post(url=url, headers=headers, data=body, verify=False)
+#         data = resp.json()["Data"][0]["TxRutaPolitico"].split("partidario_",1)[1]
+#         data_encoded = urllib.parse.quote(data.encode('utf8'))
+#         dict[dni_candidato] = data_encoded
+#         print(data_encoded)
+#     except:
+#         continue
+# df = pd.DataFrame(dict.items(), columns=["DNI","URL"])
+# df.to_csv("dnis_urls_alcaldes_provinciales.csv")
 
 # HISTORIAL PARTIDARIO
 
-candidatos = pd.read_csv('dnis_urls_alcaldes_provinciales.csv', index_col=0)
+# candidatos = pd.read_csv('dnis_urls_alcaldes_provinciales.csv', index_col=0)
 
-dict = {}
-for dni_candidato, url_candidato in candidatos.itertuples(index=False):
-    dict[dni_candidato] = {}
-    try:
-        body = {"token": "KOlSDC8AUxc=lC"}
-        r = session.post(f"https://infogob.jne.gob.pe/Politico/_HistorialFichaPolitico?istrParameters={url_candidato}", verify=False, data=body)
+# dict = {}
+# for dni_candidato, url_candidato in candidatos.itertuples(index=False):
+#     dict[dni_candidato] = {}
+#     try:
+#         body = {"token": "KOlSDC8AUxc=lC"}
+#         r = session.post(f"https://infogob.jne.gob.pe/Politico/_HistorialFichaPolitico?istrParameters={url_candidato}", verify=False, data=body)
 
-        about = r.html.find('.alias')
-        for idx, partido in enumerate(about):
-            dict[dni_candidato][idx] = partido.text
+#         about = r.html.find('.alias')
+#         for idx, partido in enumerate(about):
+#             dict[dni_candidato][idx] = partido.text
 
-    except:
-        continue
+#     except:
+#         continue
 
-df = pd.DataFrame.from_dict(dict, orient="index")
-df.to_csv("historial_partidario_alcaldes_provinciales.csv")
+# df = pd.DataFrame.from_dict(dict, orient="index")
+# df.to_csv("historial_partidario_alcaldes_provinciales.csv")
 
 # PROCESOS ELECTORALES
 
