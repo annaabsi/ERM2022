c65fed06a58ffd0a6b98ac375c2c595c4db651cd
Anna Absi <anna.absi@gmail.com>
2022-10-03 02:01:25 -0500

    adding host

--- scraper.py
+++ scraper.py
@@ -3,7 +3,8 @@ import pandas as pd
 
 url = "https://api.resultadoserm2022.onpe.gob.pe/results/03/140100"
 headers = {
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:66.0) Gecko/20100101 Firefox/66.0"}
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:66.0) Gecko/20100101 Firefox/66.0",
+    "Host": "api.resultadoserm2022.onpe.gob.pe"}
 resp = requests.get(url, headers=headers)
 print (resp)
 print (resp.text)
