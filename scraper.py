7ceb7da1ed8b2dc82a8ffd4fad4fcb48dfd5014f
Anna Absi <anna.absi@gmail.com>
2022-10-03 02:09:06 -0500

    more headers

--- scraper.py
+++ scraper.py
@@ -3,6 +3,8 @@ import pandas as pd
 
 url = "https://api.resultadoserm2022.onpe.gob.pe/results/03/140100"
 headers = {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:66.0) Gecko/20100101 Firefox/66.0",
     "Host": "api.resultadoserm2022.onpe.gob.pe"}
 resp = requests.get(url, headers=headers)
